3065739653bce5eff252a665b3dfb3259fab4531
Bavo <b4vom4es@gmail.com>
2018-11-07 14:23:57 +0100

    Static home

--- fisearch-app/src/app/app.module.ts
+++ fisearch-app/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { FormsModule } from '@angular/forms';
 import { NavigationModule } from './navigation/navigation.module';
 import { LoginModule } from './login/login.module';
 import { UploadModule } from './upload/upload.module';
+import { HomeModule } from './home/home.module';
 
 import { AppComponent } from './app.component';
 import { MainLoginComponent } from './login/main-login/main-login.component';
 import { SingleUploadComponent } from './upload/single-upload/single-upload.component';
+import { MainHomeComponent } from './home/main-home/main-home.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'login', component: MainLoginComponent },
   { path: 'upload', component: SingleUploadComponent },
+  { path: '', component: MainHomeComponent },
 ];
 
 @NgModule({
@@ -27,6 +30,7 @@ const appRoutes: Routes = [
     LoginModule,
     NavigationModule,
     UploadModule,
+    HomeModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true }
---
+++ fisearch-app/src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainHomeComponent } from './main-home/main-home.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainHomeComponent]
+})
+export class HomeModule { }
